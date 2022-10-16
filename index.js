cdebda15a821789a6074363c4f06720207248460
deyanazhari <deyanazhari@gmail.com>
2022-10-17 01:02:37 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
-
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(5000, ()=> console.log('Server up and running...'));
+app.listen(port, ()=> console.log('Server up and running...'));
